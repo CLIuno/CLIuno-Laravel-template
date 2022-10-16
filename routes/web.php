fe72cecb8cc6148d39a635be941d1ac8b9462478
RuM <18311071+ru44@users.noreply.github.com>
2022-10-17 00:55:43 +0300

    psql

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return ['Laravel' => app()->version()];
+    return \App\Models\User::first()->name;
 });
 
 require __DIR__.'/auth.php';
